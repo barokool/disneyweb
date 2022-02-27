addd849eaf26c40980b4e371459a223d2144ef16
baro gia <knabao7a7@gmail.com>
2022-02-27 20:38:26 +0700

    removed react-slick && slick carousel, using swiper instead

--- src/pages/detail/Detail.jsx
+++ src/pages/detail/Detail.jsx
@@ -153,16 +153,45 @@ const MoviesDesc = styled.div`
 `;
 
 const ButtonWatch = styled.button`
-  width: 100%;
-  max-width: 300px;
-  border-radius: 10px;
-  padding: 1rem 0rem;
-  font-size: 20px;
-  font-weight: 600;
+  appearance: none;
+  background-color: #fcfcfc;
+  border: 2px solid #1A1A1A;
+  border-radius: 15px;
+  box-sizing: border-box;
+  color: black;
   cursor: pointer;
-  &:hover {
-    opacity: 0.8;
-  }
+  display: inline-block;
+  font-family: Roobert,-apple-system,BlinkMacSystemFont,"Segoe UI",Helvetica,Arial,sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol";
+  font-size: 16px;
+  font-weight: 600;
+  line-height: normal;
+  margin: 0;
+  min-height: 60px;
+  max-width:300px;
+  outline: none;
+  padding: 16px 24px;
+  text-align: center;
+  text-decoration: none;
+  transition: all 300ms cubic-bezier(.23, 1, 0.32, 1);
+  user-select: none;
+  -webkit-user-select: none;
+  touch-action: manipulation;
+  width: 100%;
+  will-change: transform;
+
+&:disabled {
+  pointer-events: none;
+}
+
+&:hover {
+  box-shadow: rgba(0, 0, 0, 0.25) 0 8px 15px;
+  transform: translateY(-2px);
+}
+
+&:active {
+  box-shadow: none;
+  transform: translateY(0);
+}
 `;
 
 const ContainerButton = styled.div`
